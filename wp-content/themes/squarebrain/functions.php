683b1692525203257a67f01ee482ee00e530bb1b
fahadbbil <hello@MacBooks-MacBook-Pro.local>
2020-08-27 15:31:15 +0600

    Woocommerce css, js dequeue where it is not needed

--- wp-content/themes/squarebrain/functions.php
+++ wp-content/themes/squarebrain/functions.php
@@ -236,3 +236,34 @@ function page_bg(){
 
 /*Meta Box Create For About Page Template*/
 require 'inc/func_about_meta.php';
+
+/*
+ * Woocommerce css and js dequeue where not woocommerce
+ */
+
+function dequeueWocoomerceSquareBrain() {
+
+    // Check if WooCommerce plugin is active
+    if( function_exists( 'is_woocommerce' ) ){
+
+        // Check if it's any of WooCommerce page
+        if(! is_woocommerce() && ! is_cart() && ! is_checkout() && !is_page('products') ) {
+
+            ## Dequeue WooCommerce styles
+            wp_dequeue_style('woocommerce-layout');
+            wp_dequeue_style('woocommerce-general');
+            wp_dequeue_style('woocommerce-smallscreen');
+
+            ## Dequeue WooCommerce scripts
+            wp_dequeue_script('wc-cart-fragments');
+            wp_dequeue_script('woocommerce');
+            wp_dequeue_script('wc-add-to-cart');
+
+            wp_deregister_script( 'js-cookie' );
+            wp_dequeue_script( 'js-cookie' );
+
+        }
+    }
+}
+add_action( 'wp_enqueue_scripts', 'dequeueWocoomerceSquareBrain' );
+
